3c5b3e333b09dc5d33611a33541ce1dfd326315f
yu7400ki <yu7400ki@gmail.com>
2023-11-11 17:20:19 +0900

    proxy

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-const app = new Hono()
+const API_URL = "https://api.openai.iniad.org";
 
-app.get('/', (c) => c.text('Hello Hono!'))
+const proxy = new Hono();
 
-export default app
+proxy.use("*", cors());
+
+proxy.mount("/", async (req) => {
+  const path = new URL(req.url).pathname;
+  const url = API_URL + path;
+  const { method, headers, body } = req;
+  const response = await fetch(url, {
+    method,
+    headers,
+    body,
+  });
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: response.headers,
+  });
+});
+
+export default proxy;
